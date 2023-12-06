6652496a43383890baf0d22f6937358c6e094193
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-06 14:32:49 -0300

    working on

--- Filter/events-list.py
+++ Filter/events-list.py
@@ -14,3 +14,12 @@ def filter_duplicate_events(events):
         if event_key not in seen_events:
             seen_events.add(event_key)
             unique_events.append(event)
+
+    return unique_events
+
+events_data = [
+    {"title": "Rocket de Laval vs. Belleville Senators", "date": "Dec 6", "time": "Wed • 7:00pm" },
+    {"title": "Montreal Canadiens vs. Los Angeles Kings", "date": "Dec 7", "time": "Thu • 7:00pm" },
+    {"title": "Rocket de Laval vs. Belleville Senators", "date": "Dec 6", "time": "Wed • 7:00pm" },
+    {"title": "Rocket de Laval vs. Hartford Wolf Pack", "date": "Dec 8", "time": "Fri • 7:00pm"}
+]
