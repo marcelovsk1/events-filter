35477c1b74171c28b71da1ecd9f10c05e8e063ed
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-07 16:53:34 -0300

    working on

--- Filter/events-filter.py
+++ Filter/events-filter.py
@@ -1,15 +1,16 @@
 class Event:
-    def __init__(self, title, date, time):
+    def __init__(self, title, date, startTime, endTime):
         self.title = title
         self.date = date
-        self.time = time
+        self.startTime = startTime
+        self.endTime = endTime
 
 def filter_duplicate_events(events):
     unique_events = []
     seen_events = set()
 
     for event in events:
-        event_key = (event.title, event.date, event.time)
+        event_key = ( event.date, event.startTime, event.endTime )
 
         if event_key not in seen_events:
             seen_events.add(event_key)
@@ -18,15 +19,15 @@ def filter_duplicate_events(events):
     return unique_events
 
 events_data = [
-    {"title": "Rocket de Laval vs. Belleville Senators", "date": "Dec 6", "time": "Wed • 7:00pm" },
-    {"title": "Montreal Canadiens vs. Los Angeles Kings", "date": "Dec 7", "time": "Thu • 7:00pm" },
-    {"title": "Rocket de Laval vs. Belleville Senators", "date": "Dec 6", "time": "Wed • 7:00pm" },
-    {"title": "Rocket de Laval vs. Hartford Wolf Pack", "date": "Dec 8", "time": "Fri • 7:00pm"}
+    {"title": "Rocket de Laval vs. Belleville Senators", "date": "Dec 6", "startTime": "Wed • 7:00pm", "endTime": "Wed • 11:59pm" },
+    {"title": "Montreal Canadiens vs. Los Angeles Kings", "date": "Dec 7", "startTime": "Thu • 7:00pm", "endTime": "Thu • 10pm" },
+    {"title": "Rocket de Laval vs. Belleville Senators", "date": "Dec 6", "startTime": "Wed • 7:00pm", "endTime": "Wed • 11pm" },
+    {"title": "Rocket de Laval vs. Hartford Wolf Pack", "date": "Dec 8", "startTime": "Fri • 7:00pm", "endTime": "Wed • 9:30pm"}
 ]
 
-events = [Event(event["title"], event["date"], event["time"]) for event in events_data]
+events = [Event(event["title"], event["date"], event["startTime"], event["endTime"]) for event in events_data]
 
 filtered_events = filter_duplicate_events(events)
 
 for event in filtered_events:
-    print(f"Title: {event.title}, Date: {event.date}, Time: {event.time}")
+    print(f"Title: {event.title}, Date: {event.date}, startTime: {event.startTime}, endTime: {event.endTime}")
